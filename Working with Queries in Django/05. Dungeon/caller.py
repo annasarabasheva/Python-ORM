aa59778ed610082825a5efbab8c53ccc5f45f8a3
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-09-25 20:54:18 +0300

    Add Dungeon Exercise

---
+++ Working with Queries in Django/05. Dungeon/caller.py
@@ -0,0 +1,159 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer, Meal, Dungeon
+
+
+def show_highest_rated_art() -> str:
+    best_artwork = ArtworkGallery.objects.order_by('-rating', 'id').first()
+
+    return f"{best_artwork.art_name} is the highest-rated art with a {best_artwork.rating} rating!"
+
+
+def bulk_create_arts(first_art, second_art) -> None:
+    ArtworkGallery.objects.bulk_create([
+        first_art,
+        second_art,
+    ])
+
+
+def delete_negative_rated_arts() -> None:
+    ArtworkGallery.objects.filter(rating__lt=0).delete()
+
+
+def show_the_most_expensive_laptop() -> str:
+    best_laptop = Laptop.objects.order_by('-price', '-id').first()
+
+    return f"{best_laptop.brand} is the most expensive laptop available for {best_laptop.price}$!"
+
+
+def bulk_create_laptops(*args):
+    Laptop.objects.bulk_create(*args)
+
+
+def update_to_512_GB_storage() -> None:
+    Laptop.objects.filter(brand__in=["Asus", "Lenovo"]).update(storage=512)
+
+
+def update_to_16_GB_memory() -> None:
+    Laptop.objects.filter(brand__in=["Apple", "Dell", "Acer"]).update(memory=16)
+
+
+def update_operation_systems() -> None:
+
+    Laptop.objects.filter(brand="Asus").update(operation_system="Windows")
+    Laptop.objects.filter(brand="Apple").update(operation_system="MacOS")
+    Laptop.objects.filter(brand__in=["Dell", "Acer"]).update(operation_system="Linux")
+    Laptop.objects.filter(brand="Lenovo").update(operation_system="Chrome OS")
+
+
+def delete_inexpensive_laptops() -> None:
+    Laptop.objects.filter(price__lt=1200).delete()
+
+
+def bulk_create_chess_players(*args) -> None:
+    ChessPlayer.objects.bulk_create(*args)
+
+
+def delete_chess_players() -> None:
+    ChessPlayer.objects.filter(title="no title").delete()
+
+
+def change_chess_games_won() -> None:
+    ChessPlayer.objects.filter(title="GM").update(games_won=30)
+
+
+def change_chess_games_lost() -> None:
+    ChessPlayer.objects.filter(title="no title").update(games_lost=25)
+
+
+def change_chess_games_drawn() -> None:
+    ChessPlayer.objects.update(games_drawn=10)
+
+
+def grand_chess_title_GM() -> None:
+    ChessPlayer.objects.filter(rating__gte=2400).update(title="GM")
+
+
+def grand_chess_title_IM() -> None:
+    ChessPlayer.objects.filter(rating__range=[2300, 2399]).update(title="IM")
+
+
+def grand_chess_title_FM() -> None:
+    ChessPlayer.objects.filter(rating__range=[2200, 2299]).update(title="FM")
+
+
+def grand_chess_title_regular_player() -> None:
+    ChessPlayer.objects.filter(rating__range=[0, 2199]).update(title="regular player")
+
+
+def set_new_chefs() -> None:
+    Meal.objects.filter(meal_type="Breakfast").update(chef="Gordon Ramsay")
+    Meal.objects.filter(meal_type="Lunch").update(chef="Julia Child")
+    Meal.objects.filter(meal_type="Dinner").update(chef="Jamie Oliver")
+    Meal.objects.filter(meal_type="Snack").update(chef="Thomas Keller")
+
+
+def set_new_preparation_times() -> None:
+    Meal.objects.filter(meal_type="Breakfast").update(preparation_time="10 minutes")
+    Meal.objects.filter(meal_type="Lunch").update(preparation_time="12 minutes")
+    Meal.objects.filter(meal_type="Dinner").update(preparation_time="15 minutes")
+    Meal.objects.filter(meal_type="Snack").update(preparation_time="5 minutes")
+
+
+def update_low_calorie_meals() -> None:
+    Meal.objects.filter(meal_type__in=["Breakfast", "Dinner"]).update(calories=400)
+
+
+def update_high_calorie_meals() -> None:
+    Meal.objects.filter(meal_type__in=["Lunch", "Snack"]).update(calories=700)
+
+
+def delete_lunch_and_snack_meals() -> None:
+    Meal.objects.filter(meal_type__in=["Lunch", "Snack"]).delete()
+
+
+def show_hard_dungeons() -> str:
+    dungeons = Dungeon.objects.filter(difficulty="Hard").order_by('-location')
+    result = [f"{dungeon.name} is guarded by {dungeon.boss_name} who has {dungeon.boss_health} health points!" for dungeon in dungeons]
+    return '\n'.join(result)
+
+
+def bulk_create_dungeons(*args) -> None:
+    Dungeon.objects.bulk_create(*args)
+
+
+def update_dungeon_names() -> None:
+    Dungeon.objects.filter(difficulty="Hard").update(name="The Lost Haunt")
+    Dungeon.objects.filter(difficulty="Medium").update(name="The Coral Labyrinth")
+    Dungeon.objects.filter(difficulty="Easy").update(name="The Erased Thombs")
+
+
+def update_dungeon_bosses_health() -> None:
+    Dungeon.objects.exclude(difficulty="Easy").update(boss_health=500)
+
+
+def update_dungeon_recommended_levels() -> None:
+    Dungeon.objects.filter(difficulty="Easy").update(recommended_level=25)
+    Dungeon.objects.filter(difficulty="Medium").update(recommended_level=50)
+    Dungeon.objects.filter(difficulty="Hard").update(recommended_level=75)
+
+
+def update_dungeon_rewards() -> None:
+    Dungeon.objects.filter(boss_health=500).update(reward="1000 Gold")
+    Dungeon.objects.filter(location__startswith="E").update(reward="New dungeon unlocked")
+    Dungeon.objects.filter(location__endswith="s").update(reward="Dragonheart Amulet")
+
+
+def set_new_locations() -> None:
+    Dungeon.objects.filter(recommended_level=25).update(location="Enchanted Maze")
+    Dungeon.objects.filter(recommended_level=50).update(location="Grimstone Mines")
+    Dungeon.objects.filter(recommended_level=75).update(location="Shadowed Abyss")
+
+
+
+

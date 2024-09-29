bd014248f41a7652089e6f752ff15bbb1c54c80d
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-09-29 14:17:16 +0300

    Add Character Classes Exercise

---
+++ Models Inheritance and Customization/01. Character Classes/caller.py
@@ -0,0 +1,9 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+from main_app.models import Mage, Necromancer
+
